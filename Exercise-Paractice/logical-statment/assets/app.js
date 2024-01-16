11917208357793ecae91aaa4e6fa674f43be4c7c
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-17 00:09:10 +0500

    exercise paractice compelete

--- Exercise-Paractice/logical-statment/assets/app.js
+++ Exercise-Paractice/logical-statment/assets/app.js
@@ -28,31 +28,62 @@ console.log(`Answer: ${answer} Response: ${response}`);
 // console.log(finalRequest);
 
 // EXERCISE 4.4
-let rendomNumber = Math.floor(Math.random() * 5);
-let userInput = prompt("Enter the Game");
-let userMessage = "";
-switch (rendomNumber) {
-    case 0:
-        userMessage = "igi";
-        break;
+// let rendomNumber = Math.floor(Math.random() * 5);
+// let userInput = prompt("Enter the Game");
+// let userMessage = "";
+// switch (rendomNumber) {
+//     case 0:
+//         userMessage = "igi";
+//         break;
+//     case 1:
+//         userMessage = "8-ball";
+//         break;
+//     case 2:
+//         userMessage = "snooker";
+//         break;
+//     case 3:
+//         userMessage = "chess";
+//         break;
+//     case 4:
+//         userMessage = "carrom";
+//         break;
+//     case 5:
+//         userMessage = "GTA";
+//         break;
+//     default:
+//         userMessage = "not play game";
+//         break;
+// }
+// let find = `UserInput: ${userInput} ComputerGenrateGame: ${userMessage}`;
+// console.log(find);
+
+// EXERCISE 4.5
+let prize = +prompt("Enter The Number 1 to 10");
+let mySelection = "";
+switch (prize) {
     case 1:
-        userMessage = "8-ball";
-        break;
     case 2:
-        userMessage = "snooker";
+        mySelection = "Mubarak! ho Apne Toyota car jeeta he";
         break;
     case 3:
-        userMessage = "chess";
-        break;
     case 4:
-        userMessage = "carrom";
+        mySelection = "Mubarak! ho Apne Mheran car jeeta he";
         break;
     case 5:
-        userMessage = "GTA";
+    case 6:
+        mySelection = "Mubarak! ho Apne FX car jeeta he";
+        break;
+    case 7:
+    case 8:
+        mySelection = "Mubarak! ho Apne super bike jeeta he";
+        break;
+    case 9:
+    case 10:
+        mySelection = "Mubarak! ho Apne cicyle jeeta he";
         break;
     default:
-        userMessage = "not play game";
+        mySelection = "TRY AGAIN";
         break;
 }
-let find = `UserInput: ${userInput} ComputerGenrateGame: ${userMessage}`;
-console.log(find);
+let finalFind = `User: ${prize} Selection: ${mySelection}`;
+console.log(finalFind);
