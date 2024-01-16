f72f45623274261dfc909f4b5fbf10410efbabe6
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-16 22:34:50 +0500

    paractice

--- Exercise-Paractice/logical-statment/assets/app.js
+++ Exercise-Paractice/logical-statment/assets/app.js
@@ -5,3 +5,24 @@ if (!bool) {
 } else {
     console.log("close the umbrella");
 }
+
+// EXERCISE 4.2
+let userAge = parseInt(prompt("Enter the Age"));
+let message = "";
+if (userAge >= 21) {
+    message = "purchase alcohol";
+} else if (userAge >= 19) {
+    message = "not purchase alcohol";
+} else {
+    message = "not entry"
+}
+console.log(message);
+
+// EXERCISE 2.3
+// let bool = false;
+let response = "";
+let answer = !bool ? response = "allow" : response = "not";
+console.log(`Answer: ${answer} Response: ${response}`);
+
+let finalRequest = userAge >= 20 ? answer = "true" : answer = "false";
+console.log(finalRequest);
