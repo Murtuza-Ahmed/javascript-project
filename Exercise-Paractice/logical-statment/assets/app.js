7dbee5750de1f41887d04f64333a1df0e402682d
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-17 09:33:01 +0500

    project paractice

--- Exercise-Paractice/logical-statment/assets/app.js
+++ Exercise-Paractice/logical-statment/assets/app.js
@@ -58,32 +58,77 @@ console.log(`Answer: ${answer} Response: ${response}`);
 // console.log(find);
 
 // EXERCISE 4.5
-let prize = +prompt("Enter The Number 1 to 10");
-let mySelection = "";
-switch (prize) {
-    case 1:
-    case 2:
-        mySelection = "Mubarak! ho Apne Toyota car jeeta he";
+// let prize = +prompt("Enter The Number 1 to 10");
+// let mySelection = "";
+// switch (prize) {
+//     case 1:
+//     case 2:
+//         mySelection = "Mubarak! ho Apne Toyota car jeeta he";
+//         break;
+//     case 3:
+//     case 4:
+//         mySelection = "Mubarak! ho Apne Mheran car jeeta he";
+//         break;
+//     case 5:
+//     case 6:
+//         mySelection = "Mubarak! ho Apne FX car jeeta he";
+//         break;
+//     case 7:
+//     case 8:
+//         mySelection = "Mubarak! ho Apne super bike jeeta he";
+//         break;
+//     case 9:
+//     case 10:
+//         mySelection = "Mubarak! ho Apne cicyle jeeta he";
+//         break;
+//     default:
+//         mySelection = "TRY AGAIN";
+//         break;
+// }
+// let finalFind = `User: ${prize} Selection: ${mySelection}`;
+// console.log(finalFind);
+
+// CHAPTER PROJECT
+// let dynamicNumber = Math.floor(Math.random() * 10);
+// let userNumber = +prompt("Enter the Number 0 to 10");
+// let findHere = "";
+// if (dynamicNumber > userNumber) {
+//     findHere = "Chota He";
+// } else if (dynamicNumber < userNumber) {
+//     findHere = "Bara He";
+// } else if (dynamicNumber === userNumber) {
+//     findHere = "BaraBar He";
+// } else {
+//     findHere = "Please Input Number";
+// }
+// // dynamicNumber <= userNumber ? findHere = "bara he" : findHere = "chota he";
+// let finalFindHere = `DynamicNumber: ${dynamicNumber} ApkaNumber: ${findHere} ${userNumber}`;
+// console.dir(finalFindHere);
+
+// Friend checker game
+let userFriend = prompt("Enter the Friend Name");
+friendLower = userFriend.toLowerCase();
+let friendIn = "";
+switch (friendLower) {
+    case "murtuza":
+        friendIn = "Murtuza-He";
         break;
-    case 3:
-    case 4:
-        mySelection = "Mubarak! ho Apne Mheran car jeeta he";
+    case "hamza":
+        friendIn = "Hamza-He";
         break;
-    case 5:
-    case 6:
-        mySelection = "Mubarak! ho Apne FX car jeeta he";
+    case "umer":
+        friendIn = "Umer-He";
         break;
-    case 7:
-    case 8:
-        mySelection = "Mubarak! ho Apne super bike jeeta he";
+    case "zubair":
+        friendIn = "Zubair-He";
         break;
-    case 9:
-    case 10:
-        mySelection = "Mubarak! ho Apne cicyle jeeta he";
+    case "talha":
+    case "owais":
+        friendIn = "Friend-He";
         break;
     default:
-        mySelection = "TRY AGAIN";
+        friendIn = "No friend found";
         break;
 }
-let finalFind = `User: ${prize} Selection: ${mySelection}`;
-console.log(finalFind);
+let finalFriend = `Apka Dost ${friendIn}`
+console.log(finalFriend);
