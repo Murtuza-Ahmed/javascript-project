1c092afb8e94ff199922edde11bc260870cba6db
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-20 18:19:02 +0500

    paractice

--- Exercise-Paractice/Built-In JavaScript Methods/app.js
+++ Exercise-Paractice/Built-In JavaScript Methods/app.js
@@ -7,3 +7,13 @@ const unCodeedUrl = decodeURIComponent(faceBook);
 const enGit = encodeURIComponent(gitHub);
 const unGit = decodeURIComponent(gitHub);
 console.log(`ENCODE: ${enCodedUrl} \n UNCODE: ${unCodeedUrl} \n GITHUB-ENCODE: ${enGit} \n GITHUB-UNCODE: ${unGit}`);
+
+// EXERCISE 8.2
+let friendNameArr = ["murtuza", "raza", "murtuza", "hamza", "hamza", "zubair", "raza", "umer", "sheroz"];
+let filter = friendNameArr.filter((value, index) => {
+    if (value === index) {
+        return true;
+    }
+})
+console.log(filter)
+console.log(friendNameArr)
