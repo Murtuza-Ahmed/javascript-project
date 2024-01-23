970b167c90d46179a3f52ca94e74d8e6d8a9cff4
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-23 12:42:56 +0500

    9.1 compelete

---
+++ Exercise-Paractice/The Document Object Model/app.js
@@ -0,0 +1,9 @@
+// EXERCISE 9.1
+console.dir(window);
+let documentObject = window.document;
+console.log(`documentObject: ${documentObject}`);
+let innerHeightW = window.innerHeight;
+let innerWidthW = window.innerWidth;
+let outerHeightW = window.outerHeight;
+let outerWidthW = window.outerWidth;
+console.log(`InnerHeight: ${innerHeightW} \n InnerWidth: ${innerWidthW} \n OuterHeight: ${outerHeightW} \n OuterWidth: ${outerWidthW}`);
