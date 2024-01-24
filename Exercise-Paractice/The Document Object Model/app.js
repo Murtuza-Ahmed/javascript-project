b86848de0476035a0c4b7fd126efe760541332cb
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-24 11:51:33 +0500

    project compelete

--- Exercise-Paractice/The Document Object Model/app.js
+++ Exercise-Paractice/The Document Object Model/app.js
@@ -20,12 +20,30 @@ let diCode = decodeURIComponent(her);
 console.log(diCode);
 
 // EXERCISE 9.3
-let inputClass = document.querySelector("div");
-inputClass.textContent = "The Paractice of DOM";
-inputClass.classList.add("red");
-inputClass.id = "tester";
-inputClass.style.backgroundColor = "red";
-inputClass.style.color = "#fff";
-let documentUrl = document.URL;
-console.log(documentUrl);
-console.log(inputClass)
+// let inputClass = document.querySelector("div");
+// inputClass.textContent = "The Paractice of DOM";
+// inputClass.classList.add("red");
+// inputClass.id = "tester";
+// inputClass.style.backgroundColor = "red";
+// inputClass.style.color = "#fff";
+// let documentUrl = document.URL;
+// console.log(documentUrl);
+// console.log(inputClass)
+
+// Chapter project
+let divOutPut = document.getElementsByClassName("output");
+let mainList = {
+    uniqe: document.querySelector("ul")
+}
+mainList.uniqe.id = "list";
+let divTagName = document.querySelectorAll("div");
+let textArray = [];
+let text = divTagName.innerText;
+textArray.push(text);
+for (let i = 0; i <= divTagName.length; i++) {
+    let currentDiv = divTagName[i];
+    currentDiv.id = i + 1;
+    currentDiv.style.color = i % 2 === 0 ? "red" : "blue";
+    console.log(currentDiv)
+
+}
