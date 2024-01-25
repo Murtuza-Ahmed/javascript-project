801be8484c8366d63ff7654f44aa5071b7edff52
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-25 11:53:56 +0500

    exercise 11.2 | 11.3 compelete

--- Exercise-Paractice/Interactive Content and Event Listeners/app.js
+++ Exercise-Paractice/Interactive Content and Event Listeners/app.js
@@ -13,3 +13,30 @@ function toggleDarkMode() {
     }
     darkMode = !darkMode;
 }
+
+// EXERCISE 11.2
+function changeBodyColor(color) {
+    document.body.style.backgroundColor = color;
+}
+let colorDiv = document.querySelectorAll(".colorDiv");
+colorDiv.forEach((colorDiv) => {
+    // console.log(colorDiv)
+    colorDiv.addEventListener("click", function () {
+        let colorName = this.textContent.toLowerCase();
+        // console.log(colorName)
+        changeBodyColor(colorName);
+    });
+});
+
+
+// exercise 11.3    (Attach window onload event)
+function message(msg, event) {
+    console.log(event, msg);
+};
+window.onload = function (event) {
+    message("Window Ready", event);
+};
+function domContentLoadedHandler(event) {
+    message("Document Ready", event);
+}
+document.addEventListener("DOMContentLoaded", domContentLoadedHandler);
