234cc13b0d0b39f19839ce6af742ea5f80205aa6
Murtuza-Ahmed <murtuza.programmer@gmail.com>
2024-01-27 11:45:46 +0500

    paractice

--- Exercise-Paractice/Intermediate JavaScript/app.js
+++ Exercise-Paractice/Intermediate JavaScript/app.js
@@ -1,23 +1,23 @@
 // EXERCISE 12.1
-// let outputElement = document.getElementById("output");
-// let searchInput = document.getElementById("stext");
-// let replaceInput = document.getElementById("rtext");
-// let button = document.querySelector("button");
-// button.addEventListener("click", lookUp);
-// function lookUp() {
-//     let s = outputElement.textContent;
-//     let rt = replaceInput.value;
-//     let regex = new RegExp(searchInput, "g");
-//     let matches = s.match(regex);
-//     if (matches) {
-//         let updateText = s.replace(regex, rt);
-//         outputElement.textContent = updateText
-//     }
-// }
+let outputEle = document.getElementById("output");
+let searchInput = document.getElementById("stext");
+let replaceInput = document.getElementById("rtext");
+let button = document.getElementById("button");
+button.addEventListener("click", lookUp);
+function lookUp() {
+    let s = outputEle.textContent;
+    let rt = replaceInput.value;
+    let regex = new RegExp(searchInput, "g");
+    let matches = s.match(regex);
+    if (matches) {
+        let updateText = s.replace(regex, rt);
+        outputEle.textContent = updateText
+    }
+}
 
 // EXERCISE 12.2
 let outputElement = document.querySelector(".output");
-let inputElement = document.querySelector("input[type='text']");
+let inputElement = document.getElementById("email");
 let buttonCheck = document.querySelector("button");
 buttonCheck.addEventListener("click", () => {
     let inputEmail = inputElement.value;
